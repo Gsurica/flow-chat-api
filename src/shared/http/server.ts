221b671b6d45d144a1e35b2d6ae1ec41a00664f3
gsurica <eosurica@gmail.com>
2022-10-18 15:40:17 -0300

    adding socket.io server logic

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,26 +5,12 @@ import cors from 'cors';
 import { routes } from './routes';
 import { AppError } from '@shared/errors/AppError';
 import http from 'http';
-// import socket from 'socket.io';
+import { ServerSocket } from '@shared/socket/Socket';
 
 const app = express();
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 
-// const io = socket(server, {
-//   path: '/socket.io',
-// });
-
-// const clients = [];
-
-// io.on('connection', client => {
-//   console.log(`Client connected ${client.id}`);
-//   clients.push(client);
-
-//   client.on('disconnect', () => {
-//     clients.splice(clients.indexOf(client), 1);
-//     console.log('Client disconnected!');
-//   });
-// });
+new ServerSocket(httpServer);
 
 app.use(cors());
 app.use(express.json());
